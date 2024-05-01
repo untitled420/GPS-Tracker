35caacc1e0e68f04665fdfe66fcdc5effb14baea
A-Afifi <168368644+A-Afifi@users.noreply.github.com>
2024-05-01 17:45:42 +0300

    SYSTICK.C (Part 2 )

--- GPS_Trcaker_proj/MCAL/Systick/Systick.c
+++ GPS_Trcaker_proj/MCAL/Systick/Systick.c
@@ -14,3 +14,26 @@ void Systick_INIT(void){
 	NVIC_ST_CTRL_R = 0x05;				//enabling the systick time & and the source clk bits
 
 }
+
+
+void Systick_wait(uint32_t delay){
+	NVIC_ST_RELOAD_R = delay-1;			// delay-1 to handle the zero cycle
+	NVIC_ST_CURRENT_R = 0;				// to empty the current value register
+	while((NVIC_ST_CTRL_R & 0x00010000)==0){}; // wait until the counter finish counting and the Count flag is fired
+}
+
+void delay_ms(uint32_t count){
+		//itirations for number of counts the user is deciding
+		uint32_t i;
+		for(i=0; i<count; i++){
+			Systick_wait(16000);
+		}
+}
+
+void delay_us(uint32_t count){
+		uint32_t i;
+		for(i=0; i<count; i++){
+			Systick_wait(16);
+		}
+}
+
