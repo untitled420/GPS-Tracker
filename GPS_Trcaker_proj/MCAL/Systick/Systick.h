e353a0e5f65e2f0b82c56bbd85b22a4e2acfb5e8
A-Afifi <168368644+A-Afifi@users.noreply.github.com>
2024-05-01 17:38:48 +0300

    SYSTICK.h (Part 1)

---
+++ GPS_Trcaker_proj/MCAL/Systick/Systick.h
@@ -0,0 +1,21 @@
+/*
+ * Systick.h
+ *
+ * Created: 4/21/2024 
+ *  Author: Ahmed & Andrew 
+ */ 
+
+
+#ifndef SYSTICK_H_
+#define SYSTICK_H_
+
+#include <stdint.h>
+#include "common_macros.h"
+#include "tm4c123gh6pm.h"
+
+
+/****************************************************************************************************************
+												Some imprtant defintions
+****************************************************************************************************************/
+
+#define preload_value		0x00000000		//
