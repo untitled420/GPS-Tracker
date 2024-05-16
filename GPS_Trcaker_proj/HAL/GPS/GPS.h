5c3deead8a8d879edc3f0ca86181d3e77f7e6ed9
Abdelrahman-Emad03 <168363727+Abdelrahman-Emad03@users.noreply.github.com>
2024-05-16 21:05:54 +0300

    Update GPS.h

--- GPS_Trcaker_proj/HAL/GPS/GPS.h
+++ GPS_Trcaker_proj/HAL/GPS/GPS.h
@@ -43,3 +43,42 @@ float ToDEGREE(float angle);
  * it has 1 argument : arg 1 -> the angle in Degrees
 */
 float ToRAD(float angle);
+
+/*
+ * Description :
+ * A Function to Extract the desired data from a sequence of data sent from the GPS 
+ * it has no arguments 
+*/
+void GPSread();
+
+/*
+ * Description :
+ * A Function to calculate the distance between two consective coordinates read by the GPS
+ * it has no arguments 
+*/
+float dist(void);
+
+
+
+/*
+ * Description :
+ * A Function to calculate the distance between the currnt coordinate to the destination's
+ * it has no arguments 
+*/
+float Finaldist(void);
+
+/*
+ * Description :
+ * A Function to View the Readings of the GPS on the Serial through UART 0
+ * it has no arguments 
+*/
+void GPS_test ();
+
+/*
+ * Description :
+ * A Function to View the Readings of a given number on the Serial through UART 0
+ * it has 2 arguments: arg 1 -> the string to be stored in the result, arg 2 -> the float number to be shown 
+*/
+void testing(char *str, float num);
+
+#endif
