f4886438c0b06ae952ca928a2eb6819b421450ce
shamskhaled26 <113312917+shamskhaled26@users.noreply.github.com>
2024-05-16 21:05:03 +0300

    Update GPS.h

--- GPS_Trcaker_proj/HAL/GPS/GPS.h
+++ GPS_Trcaker_proj/HAL/GPS/GPS.h
@@ -5,4 +5,41 @@
  *  Author: Abdelrahman Emad & Shams Khaled 
  */
  
- 
+
+#ifndef GPS_H_
+#define GPS_H_
+
+
+#include <stdio.h>
+#include <math.h>
+#include <string.h>
+#include <stdlib.h>
+#include "../../MCAL/UART/UART.h"
+#include "../../MCAL/EEPROM/EEPROM.h"
+
+
+/************************************************************************/
+/*           some important defintions                                  */
+/************************************************************************/
+#define PI 3.142857
+
+
+/*******************************************************************************
+ *                              Functions Prototypes                           *
+ *******************************************************************************/
+/*
+
+
+/*
+ * Description :
+ * A Function to convert Radian angle to degress
+ * it has 1 argument : arg 1 -> the angle in Radian
+*/
+float ToDEGREE(float angle);
+
+/*
+ * Description :
+ * A Function to convert  angle in Degrees into Radian
+ * it has 1 argument : arg 1 -> the angle in Degrees
+*/
+float ToRAD(float angle);
